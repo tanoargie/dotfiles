a665ef4f2f1c65e34bb53bf6684047595c0ce0be
Franco Serio <franco@samser.co>
2023-09-30 21:53:47 -0300

    first test install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,60 @@
 #!/bin/bash
+answer_is_yes() {
+    [[ "$REPLY" =~ ^[Yy]$ ]] \
+        && return 0 \
+        || return 1
+}
+print_error() {
+    # Print output in red
+    printf "\e[0;31m  [✖] $1 $2\e[0m\n"
+}
+print_success() {
+    # Print output in green
+    printf "\e[0;32m  [✔] $1\e[0m\n"
+}
+print_question() {
+    # Print output in yellow
+    printf "\e[0;33m  [?] $1\e[0m"
+}
+ask_for_confirmation() {
+    print_question "$1 (y/n) "
+    read -n 1
+    printf "\n"
+}
+declare -a FILES_TO_SYMLINK=".zshrc .skhdrc .mkshrc .tmux.conf"
+main() {
 
-cp .bashrc $HOME
-cp .mkshrc $HOME
-cp .skhdrc $HOME
-cp .tmux.conf $HOME
-cp .zshrc $HOME
+    local i=""
+    local sourceFile=""
+    local targetFile=""
+
+    for i in ${FILES_TO_SYMLINK[@]}; do
+
+        sourceFile="$(pwd)/$i"
+        targetFile="$HOME/$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+        if [ -e "$targetFile" ]; then
+            if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
+
+                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+                if answer_is_yes; then
+                    rm -rf "$targetFile"
+                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+                else
+                    print_error "$targetFile → $sourceFile"
+                fi
+
+            else
+                print_success "$targetFile → $sourceFile"
+            fi
+        else
+            execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+        fi
+
+    done
+
+}
+
+main
+
+
